5a08348d2f8132dc308b5d80e9e8f4c677bdc280
hss01248 <hss01248@gmail.com>
2018-07-15 21:00:40 +0800

    rx

---
+++ app/src/main/java/com/hss01248/demos/rx/RxDemoActivity.java
@@ -0,0 +1,18 @@
+package com.hss01248.demos.rx;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hss on 2018/7/15.
+ */
+
+public class RxDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
